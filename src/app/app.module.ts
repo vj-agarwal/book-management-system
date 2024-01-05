8c0fac65ecb47b8df287b842e7a293c69cc1a30c
Vijay Agarwal <ervjagarwal@gmail.com>
2024-01-05 22:14:25 +0530

    Commit for Navigation Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { ReservationModule } from './reservation/reservation.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +13,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HomeModule,
+    ReservationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
