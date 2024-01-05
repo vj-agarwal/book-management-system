824959d385e62cb85eb474403aa7de50a210b820
Vijay Agarwal <ervjagarwal@gmail.com>
2024-01-05 22:53:02 +0530

    CRUD Operations Added to Service File

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
-
+import { Reservation } from '../models/reservation';
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  private reservations: Reservation[] = [];
+
+  constructor() { 
+
+  }
+
+  getReservations(): Reservation[]{
+    return this.reservations;
+  }
+
+  getReservation(id:string): Reservation | undefined{
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void{
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id:string): void{
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1);
+  }
+
+  updateReservation(updatedrservation:Reservation):void{
+    let index = this.reservations.findIndex(res => res.id === updatedrservation.id);
+    this.reservations[index] = updatedrservation;
+  }
 }
